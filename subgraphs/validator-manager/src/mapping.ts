934fc127b491fb5ae42629251852a683385a8b69
Akshat Chhajer <akshat.c2k@gmail.com>
2025-02-25 20:25:24 +0530

    refactor according to acp99 spec (#3)
    * refactor according to acp99 spec
    
    * nit: add line
    
    * delete lockfile from validator-manager

--- subgraphs/validator-manager/src/mapping.ts
+++ subgraphs/validator-manager/src/mapping.ts
@@ -1,27 +1,68 @@
 import { 
-    DelegatorAdded,
-    DelegatorAddedNFT,
-    DelegatorRegistered,
-    DelegatorRemovalInitialized,
-    DelegationEnded,
-    UptimeUpdated,
-    ValidationPeriodCreated,
-    InitialValidatorCreated,
-    ValidationPeriodRegistered,
-    ValidatorRemovalInitialized,
-    ValidationPeriodEnded,
-    ValidatorWeightUpdate
-} from '../generated/ERC721TokenStakingManager/ERC721TokenStakingManager'
+    InitiatedValidatorRegistration,
+    InitiatedValidatorRemoval,
+    CompletedValidatorRegistration,
+    CompletedValidatorRemoval,
+    CompletedValidatorWeightUpdate,
+} from '../generated/ValidatorManager/ValidatorManager'
+import { 
+    InitiatedDelegatorRegistration,
+    InitiatedDelegatorRemoval,
+    CompletedDelegatorRegistration,
+    CompletedDelegatorRemoval,
+    DelegatedNFTs,
+    UptimeUpdated
+} from '../generated/Native721TokenStakingManager/Native721TokenStakingManager'
 import { 
     Validation,
     Delegation,
-    DelegationNFT,
     UptimeUpdate,
-    ValidatorWeight
 } from '../generated/schema'
+import { Bytes, BigInt } from '@graphprotocol/graph-ts'
 
-export function handleDelegatorAdded(event: DelegatorAdded): void {
-    let entity = new Delegation(event.params.delegationID)
+
+export function handleInitiatedValidatorRegistration(event: InitiatedValidatorRegistration): void {
+    let entity = getOrCreateValidation(event.params.validationID)
+
+    entity.nodeID = event.params.nodeID
+    entity.weight = event.params.weight
+    entity.status = "PendingAdded"
+    entity.save()
+}
+
+export function handleCompletedValidatorRegistration(event: CompletedValidatorRegistration): void {
+    let entity = getOrCreateValidation(event.params.validationID)
+
+    entity.startedAt = event.block.timestamp.toI64()
+    entity.status = "Active"
+    entity.save()
+}
+
+
+export function handleInitiatedValidatorRemoval(event: InitiatedValidatorRemoval): void {
+    let entity = getOrCreateValidation(event.params.validationID)
+
+    entity.endTime = event.params.endTime.toI64()
+    entity.status = "PendingRemoved"
+    entity.save()
+}
+
+export function handleCompletedValidatorRemoval(event: CompletedValidatorRemoval): void {
+    let entity = getOrCreateValidation(event.params.validationID)
+
+    entity.status = "Removed"
+    entity.save()
+}
+
+export function handleCompletedValidatorWeightUpdate(event: CompletedValidatorWeightUpdate): void {
+    let entity = getOrCreateValidation(event.params.validationID)
+
+    entity.weight = event.params.weight
+    entity.save() 
+}
+
+export function handleInitiatedDelegatorRegistration(event: InitiatedDelegatorRegistration): void {
+    let entity = getOrCreateDelegation(event.params.delegationID)
 
     entity.validationID = event.params.validationID
     entity.owner = event.params.delegatorAddress
@@ -30,53 +71,41 @@ export function handleDelegatorAdded(event: DelegatorAdded): void {
     entity.save()
 }
 
-export function handleDelegatorNFTAdded(event: DelegatorAddedNFT): void {
-    let entity = new DelegationNFT(event.params.delegationID)
-  
-    entity.validationID = event.params.validationID
-    entity.owner = event.params.delegatorAddress
-    entity.weight = event.params.delegatorWeight
+export function handleCompletedDelegatorRegistration(event: CompletedDelegatorRegistration): void {
+    let entity = getOrCreateDelegation(event.params.delegationID)
+
+    entity.startedAt = event.params.startTime.toI64()
     entity.status = "Active"
-    // entity.startedAt = event.transaction.timestamp
+    entity.save()
+}
+
+export function handleInitiatedDelegatorRemoval(event: InitiatedDelegatorRemoval): void {
+    let entity = getOrCreateDelegation(event.params.delegationID)
+
+    entity.endTime = event.block.timestamp.toI64()
+    entity.status = "PendingRemoved"
+    entity.save()
+}
+
+export function handleCompletedDelegatorRemoval(event: CompletedDelegatorRemoval): void {
+    let entity = getOrCreateDelegation(event.params.delegationID)
+
+    entity.status = "Removed"
+    entity.save()
+}
+
+export function handleDelegatedNFTs(event: DelegatedNFTs): void {
+    let entity = getOrCreateDelegation(event.params.delegationID)
+
     entity.tokenIDs = event.params.tokenIDs
     entity.save()
 }
 
-export function handleDelegatorRegistered(event: DelegatorRegistered): void {
-    let entity = Delegation.load(event.params.delegationID)
-    entity!.status = "Active"
-    entity!.startedAt = event.params.startTime.toI64()
-    entity!.save()
-}
-
-export function handleDelegatorRemovalInitialized(event: DelegatorRemovalInitialized): void {
-    let entity = Delegation.load(event.params.delegationID)
-    if (entity == null) {
-        let e = DelegationNFT.load(event.params.delegationID)
-        // e!.endedAt = event.transaction.timestamp
-        e!.status = "PendingRemoved"
-        e!.save()
-    }
-    
-    entity!.status = "PendingRemoved"
-    // entity.endedAt = event.transaction.timestamp
-    entity!.save()
-}
-
-export function handleDelegationEnded(event: DelegationEnded): void {
-    let entity = Delegation.load(event.params.delegationID)
-    if (entity == null) {
-        let e = DelegationNFT.load(event.params.delegationID)
-        e!.status = "Removed"
-        e!.save()
-    }
-
-    entity!.status = "Removed"
-    entity!.save()
-}
-
 export function handleUptimeUpdated(event: UptimeUpdated): void {
-    let entity = new UptimeUpdate(event.params.validationID.concatI32(event.params.epoch.toI32()))
+    let entity = UptimeUpdate.load(event.params.validationID.concatI32(event.params.epoch.toI32()))
+    if (entity == null) {
+        entity = new UptimeUpdate(event.params.validationID.concatI32(event.params.epoch.toI32()))
+    }
 
     entity.validationID = event.params.validationID
     entity.uptimeSeconds = event.params.uptime
@@ -84,50 +113,26 @@ export function handleUptimeUpdated(event: UptimeUpdated): void {
     entity.save()
 }
 
-export function handleValidationPeriodCreated(event: ValidationPeriodCreated): void {
-    let entity = new Validation(event.params.validationID)
-
-    entity.nodeID = event.params.nodeID
-    entity.weight = event.params.weight
-    entity.status = "PendingAdded"
-    entity.save()
+// helper funcfions
+function getOrCreateValidation(id: Bytes): Validation {
+    let entity = Validation.load(id)
+    if (entity == null) {
+        entity = new Validation(id)
+        entity.nodeID = Bytes.empty()
+        entity.weight = BigInt.zero()
+        entity.status = "Unknown"
+    }
+    return entity;
 }
 
-export function handleInitialValidatorCreated(event: InitialValidatorCreated): void {
-    let entity = new Validation(event.params.validationID)
-
-    entity.nodeID = event.params.nodeID
-    entity.weight = event.params.weight
-    entity.status = "Acitve"
-    entity.save()
-}
-
-export function handleValidationPeriodRegistered(event: ValidationPeriodRegistered): void {
-    let entity = new Validation(event.params.validationID)
-
-    entity.startedAt = event.params.timestamp.toI64()
-    entity.status = "Active"
-    entity.save()
-}
-
-export function handleValidatorRemovalInitialized(event: ValidatorRemovalInitialized): void {
-    let entity = new Validation(event.params.validationID)
-
-    entity.endTime = event.params.endTime.toI64()
-    entity.status = "PendingRemoved"
-    entity.save()
-}
-
-export function handleValidationPeriodEnded(event: ValidationPeriodEnded): void {
-    let entity = new Validation(event.params.validationID)
-
-    entity.status = "Removed"
-    entity.save()
-}
-
-export function handleValidatorWeightUpdate(event: ValidatorWeightUpdate): void {
-    let entity = new ValidatorWeight(event.params.validationID)
-
-    entity.weight = event.params.weight
-    entity.save()
+function getOrCreateDelegation(id: Bytes): Delegation {
+    let entity = Delegation.load(id)
+    if (entity == null) {
+        entity = new Delegation(id)
+        entity.validationID = Bytes.empty()
+        entity.owner = Bytes.empty()
+        entity.weight = BigInt.zero()
+        entity.status = "Unknown"
+    }
+    return entity;
 }
